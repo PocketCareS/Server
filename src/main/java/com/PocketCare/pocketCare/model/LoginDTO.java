557497117e4c3fcbfb34c35a599e360e012c329f
anujbhar <anujbhar@buffalo.edu>
2020-07-27 22:08:56 -0400

    login and contact tracing

---
+++ src/main/java/com/PocketCare/pocketCare/model/LoginDTO.java
@@ -0,0 +1,22 @@
+package com.PocketCare.pocketCare.model;
+
+public class LoginDTO {
+    String userName;
+    String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
