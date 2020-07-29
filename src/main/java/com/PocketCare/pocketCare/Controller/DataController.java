0f0b65873e3e4716c9fc2b9126a121f0b2be56d6
anujbhar <anujbhar@buffalo.edu>
2020-07-29 11:43:28 -0700

    removed dump files

--- src/main/java/com/PocketCare/pocketCare/Controller/DataController.java
+++ src/main/java/com/PocketCare/pocketCare/Controller/DataController.java
@@ -1,22 +1,27 @@
 package com.PocketCare.pocketCare.Controller;
 
-import java.io.IOException;
-
 import javax.servlet.http.HttpServletResponse;
 
-import com.PocketCare.pocketCare.Service.UserContactService;
-import com.PocketCare.pocketCare.model.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.PocketCare.pocketCare.Exception.AuthorizationException;
 import com.PocketCare.pocketCare.Exception.ClientException;
 import com.PocketCare.pocketCare.Exception.CustomException;
 import com.PocketCare.pocketCare.Service.DataService;
+import com.PocketCare.pocketCare.Service.UserContactService;
+import com.PocketCare.pocketCare.model.DailyContactRequest;
+import com.PocketCare.pocketCare.model.DataUploadRequest;
+import com.PocketCare.pocketCare.model.UserContactRequest;
 
 @RestController
 @RequestMapping("/upload")
@@ -112,28 +117,5 @@ public class DataController {
 			return new ResponseEntity<>(ex, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-
-	@RequestMapping(value = "/location", method = RequestMethod.POST)
-	public ResponseEntity<?> saveLocationData(@RequestBody DataUploadRequest dataToUpload,
-			@RequestHeader("token") String token, HttpServletResponse response) throws IOException {
-
-		try {
-			logger.debug("Location Data Controller");
-			return new ResponseEntity<>(dataService.saveLocation(token, dataToUpload), HttpStatus.OK);
-		}  catch (AuthorizationException ex) {
-			logger.error("Exception auth");
-			return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
-		} catch (ClientException ex) {
-			logger.error("Exception client", ex);
-			return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
-		} catch (CustomException ex) {
-			logger.error("Exception custom", ex);
-			return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
-		}catch (Exception ex) {
-			logger.error("Exception", ex);
-			return new ResponseEntity<>(ex, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
-
 
 }
