fa7f98e13c6df277f507778c98d9b80ce12b4881
anujbhar <anujbhar@buffalo.edu>
2020-07-29 11:43:28 -0700

    removed dump files

--- src/main/java/com/PocketCare/pocketCare/DAO/DataDAO.java
+++ src/main/java/com/PocketCare/pocketCare/DAO/DataDAO.java
@@ -1,13 +1,8 @@
 package com.PocketCare.pocketCare.DAO;
 
-import java.util.List;
-
-import com.mongodb.client.result.UpdateResult;
 import org.springframework.stereotype.Component;
 
 import com.PocketCare.pocketCare.Entities.ContactListData;
-import com.PocketCare.pocketCare.Entities.LocationData;
-import com.PocketCare.pocketCare.model.Coordinates;
 import com.mongodb.client.result.UpdateResult;
 
 @Component
@@ -15,6 +10,6 @@ public interface DataDAO {
 
 	public UpdateResult saveContact(ContactListData data);
 
-	public UpdateResult saveLocation(String deviceId, List<Coordinates> data);
+	
 
 }
--- src/main/java/com/PocketCare/pocketCare/DAOImpl/DataDAOImpl.java
+++ src/main/java/com/PocketCare/pocketCare/DAOImpl/DataDAOImpl.java
@@ -1,9 +1,5 @@
 package com.PocketCare.pocketCare.DAOImpl;
 
-import java.util.List;
-
-import com.mongodb.client.result.UpdateResult;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +12,6 @@ import org.springframework.stereotype.Repository;
 import com.PocketCare.pocketCare.DAO.DataDAO;
 import com.PocketCare.pocketCare.DAO.UserDataDAO;
 import com.PocketCare.pocketCare.Entities.ContactListData;
-import com.PocketCare.pocketCare.Entities.LocationData;
-import com.PocketCare.pocketCare.model.Coordinates;
 import com.mongodb.client.result.UpdateResult;
 
 @Repository
@@ -38,16 +32,5 @@ public class DataDAOImpl implements DataDAO {
 		update.set("contactList", data.getContactList());
 		logger.debug("Data save Contact", "updating saveContact");
 		return mongoTemplate.upsert(query, update, ContactListData.class);
-	}
-
-	@Override
-	public UpdateResult saveLocation(String deviceId, List<Coordinates> data) {
-		logger.debug("Data save location", "inside saveLocation");
-		Query query = new Query();
-        query.addCriteria(Criteria.where("deviceId").is(deviceId));
-        Update update = new Update();
-        update.set("location", data);
-        logger.debug("Data save location", "updating location");
-        return mongoTemplate.upsert(query,update,LocationData.class);
 	}
 }
